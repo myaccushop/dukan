f2bb697fa01a7b4e31c6d14deabb16dfe065d041
Mahesh Asolkar <mahesh@mahesha.com>
2010-12-22 23:51:41 -0800

    Fixed slider to work well with 960gs styles

--- landing.php
+++ landing.php
@@ -16,11 +16,11 @@
 
   require(DIR_WS_INCLUDES . 'template_top.php');
 ?>
-<link rel="stylesheet" type="text/css" href="http://look.mahesha.com/ext/jquery/lofslidernews/css/style4.css" />
+<link rel="stylesheet" type="text/css" href="http://look.mahesha.com/ext/jquery/lofslidernews/css/style6.css" />
 <script language="javascript" type="text/javascript" src="http://look.mahesha.com/ext/jquery/lofslidernews/js/jquery.js"></script>
 <script language="javascript" type="text/javascript" src="http://look.mahesha.com/ext/jquery/lofslidernews/js/jquery.easing.js"></script>
 <script language="javascript" type="text/javascript" src="http://look.mahesha.com/ext/jquery/lofslidernews/js/script.js"></script>
-<script type="text/javascript">
+<!-- script type="text/javascript">
 $(document).ready( function(){
   $obj = $('#lofslidecontent45').lofJSidernews( { interval : 4000,
     easing      : 'easeInOutQuad',
@@ -31,9 +31,33 @@ $(document).ready( function(){
     navPosition     : 'horizontal', // horizontal
     navigatorHeight : 15,
     navigatorWidth  : 25,
-    mainWidth:980} );
+    mainWidth:960} );
   });
+</script -->
+
+<script type="text/javascript">
+$(document).ready( function(){
+  var buttons = { previous  : $('#lofslidecontent45 .lof-previous') ,
+                  next      : $('#lofslidecontent45 .lof-next') };
+
+  $obj = $('#lofslidecontent45').lofJSidernews( {
+    interval        : 10000,
+    easing          : 'easeInOutQuad',
+    duration        : 1200,
+    auto            : false,
+    mainWidth       : 960,
+    navPosition     : 'horizontal', // horizontal
+    navigatorHeight : 15,
+    navigatorWidth  : 25,
+    buttons         : buttons} );
+});
 </script>
+<style>
+ul.lof-main-wapper li {
+  position:relative;
+  display: inline;
+}
+</style>
 
 <?php
   if ( (!isset($new_products_category_id)) || ($new_products_category_id == '0') ) {
@@ -48,28 +72,31 @@ $(document).ready( function(){
     $counter = 0;
     $nav_counter = 0;
 
-    $new_prods_content = '<div id="lofslidecontent45" class="lof-slidecontent" style="height:340px;"><div class="preload"><div></div></div> <div class="lof-main-outer" style="height:340px;"><ul class="lof-main-wapper">';
+    $new_prods_content = '<div id="lofslidecontent45" class="lof-slidecontent grid_24 alpha omega" style="height:340px;"><div class="preload"><div></div></div> <div class="lof-main-outer grid_24 alpha omega" style="height:340px;">' . "\n";
+    $new_prods_content .= '    <div onclick="return false" href="" class="lof-previous">Previous</div>' . "\n";
+    $new_prods_content .= '<ul class="lof-main-wapper">' . "\n";
     while ($new_products = tep_db_fetch_array($new_products_query)) {
       $counter++;
 
-# $new_prods_content .= '<td width="33%" align="center" valign="top"><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $new_products['products_image'], $new_products['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br /><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . $new_products['products_name'] . '</a><br />' . $currencies->display_price($new_products['products_price'], tep_get_tax_rate($new_products['products_tax_class_id'])) . '</td>';
-      $new_prods_content .= '<li>' . tep_image(DIR_WS_IMAGES . $new_products['products_image'], $new_products['products_name'], "980px", "340px"). "\n";
-      $new_prods_content .= '<div class="lof-main-item-desc">' . "\n";
-      $new_prods_content .= '  <h3><a target="_parent" title="Newsflash 1" href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . $new_products['products_name'] . '</a> <i>' . $currencies->display_price($new_products['products_price'], tep_get_tax_rate($new_products['products_tax_class_id'])) . '</i></h3>' . "\n";
-      $new_prods_content .= '  <h2>Content of Newsflash 1</h2>' . "\n";
-      $new_prods_content .= '  <p>The one thing about a Web site, it always changes! Joomla! makes it easy to add Articles, content,...' . "\n";
-      $new_prods_content .= '  <a class="readmore" href="#">Read more </a>'. "\n";
+      $new_prods_content .= '<li class="grid_24">' . tep_image(DIR_WS_IMAGES . $new_products['products_image'], $new_products['products_name'], "950px", "340px"). "\n";
+      $new_prods_content .= '<div class="lof-main-item-desc grid_24">' . "\n";
+      $new_prods_content .= '  <h3 class="grid_20"><a target="_parent" title="Newsflash ' . $counter . '" href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">' . $new_products['products_name'] . '</a> <i>' . $currencies->display_price($new_products['products_price'], tep_get_tax_rate($new_products['products_tax_class_id'])) . '</i></h3>' . "\n";
+      $new_prods_content .= '  <h2 class="grid_20">Content of Newsflash  ' . $counter . '</h2>' . "\n";
+      $new_prods_content .= '  <p class="grid_20">The one thing about a Web site, it always changes! Joomla! makes it easy to add Articles, content,...' . "\n";
+      $new_prods_content .= '  <a class="readmore" href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $new_products['products_id']) . '">Read more </a>'. "\n";
       $new_prods_content .= "  </p>\n";
       $new_prods_content .= "</div>\n";
       $new_prods_content .= "</li>\n";
     }
 
+    $new_prods_content .= "</ul>\n";
+    $new_prods_content .= '<div onclick="return false" href="" class="lof-next">Next</div>' . "\n";
     $new_prods_content .= "</div>\n";
 
     $new_prods_content .= "<div class=\"lof-navigator-wapper\">\n";
     $new_prods_content .= "  <div class=\"lof-navigator-outer\">\n";
     $new_prods_content .= "    <ul class=\"lof-navigator\">\n";
-    while ($nav_counter <= $counter) {
+    while ($nav_counter < $counter) {
       $nav_counter++;
       $new_prods_content .= "      <li><span>" . $nav_counter . "</span></li>\n";
     }
@@ -78,7 +105,7 @@ $(document).ready( function(){
     $new_prods_content .= "</div>\n";
 ?>
 
-  <div class="contentText">
+  <div class="contentText grid_24 alpha omega">
     <?php echo $new_prods_content; ?>
   </div>
 <?php
@@ -86,39 +113,4 @@ $(document).ready( function(){
 
   require(DIR_WS_INCLUDES . 'template_bottom.php');
   require(DIR_WS_INCLUDES . 'application_bottom.php');
-
-/*
-<div class="lof-main-outer" style="width:980px; height:340px;">
-  <ul class="lof-main-wapper">
-    <li>
-      <img src="images/thumbl_980x340.png" title="Newsflash 2" >
-      <div class="lof-main-item-desc">
-        <h3><a target="_parent" title="Newsflash 1" href="#Category-1">/ Newsflash 1 /</a> <i> \u2014 Monday, February 15, 2010 12:42</i></h3>
-        <h2>Content of Newsflash 1</h2>
-        <p>The one thing about a Web site, it always changes! Joomla! makes it easy to add Articles, content,...
-        <a class="readmore" href="#">Read more </a>
-        </p>
-      </div>
-    </li>
-
-  </ul>
-</div>
-<!-- END MAIN CONTENT -->
-<!-- NAVIGATOR -->
-<div class="lof-navigator-wapper">
-  <div class="lof-navigator-outer">
-    <ul class="lof-navigator">
-      <li><span>1</span></li>
-      <li><span>2</span></li>
-      <li><span>3</span></li>
-      <li><span>4</span></li>
-      <li><span>5</span></li>
-      <li><span>6</span></li>
-      <li><span>7</span></li>
-      <li><span>8</span></li>
-    </ul>
-  </div>
-</div>
-*/
-
 ?>
