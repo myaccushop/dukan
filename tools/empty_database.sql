df8d5a59ad92f5c759bc4da183468aa52569b153
Mahesh Asolkar <mahesh@mahesha.com>
2011-02-13 15:04:05 -0800

    Added an sql file with SQL command to empty data tables

---
+++ tools/empty_database.sql
@@ -0,0 +1,34 @@
+#
+# An SQL command to empty Data tables. Execute this in phpMyAdmin to
+# remove any shop-specific data from data tables
+#
+
+TRUNCATE `address_book`;
+TRUNCATE `categories`;
+TRUNCATE `categories_description`;
+TRUNCATE `customers`;
+TRUNCATE `customers_basket`;
+TRUNCATE `customers_basket_attributes`;
+TRUNCATE `customers_info`;
+TRUNCATE `manufacturers`;
+TRUNCATE `manufacturers_info`;
+TRUNCATE `newsletters`;
+TRUNCATE `orders`;
+TRUNCATE `orders_products`;
+TRUNCATE `orders_products_attributes`;
+TRUNCATE `orders_products_download`;
+TRUNCATE `orders_status`;
+TRUNCATE `orders_status_history`;
+TRUNCATE `orders_total`;
+TRUNCATE `products`;
+TRUNCATE `products_attributes`;
+TRUNCATE `products_attributes_download`;
+TRUNCATE `products_description`;
+TRUNCATE `products_images`;
+TRUNCATE `products_notifications`;
+TRUNCATE `products_options`;
+TRUNCATE `products_options_values`;
+TRUNCATE `products_options_values_to_products_options`;
+TRUNCATE `products_to_categories`;
+TRUNCATE `reviews`;
+TRUNCATE `reviews_description`;
