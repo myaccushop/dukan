67525e00a851ca482acd1fa420fb919e64d18006
Mahesh Asolkar <mahesh@mahesha.com>
2011-02-20 21:46:53 -0800

    Added shipping and returns text

--- includes/languages/english/shipping.php
+++ includes/languages/english/shipping.php
@@ -13,5 +13,19 @@
 define('NAVBAR_TITLE', 'Shipping & Returns');
 define('HEADING_TITLE', 'Shipping & Returns');
 
-define('TEXT_INFORMATION', 'Put here your Shipping & Returns information.');
+define('TEXT_INFORMATION', '<p>We offer only  Flat rate shipping only. Orders will be shipped using FedEx ground shipping. The shipping time is usually between 1 to 7 business days. Following maps shows *approximate transit time for ground shipping, for more detail visit FedEx website.
+
+<p><img height="664" src="https://lh5.googleusercontent.com/_MjyUohgdlQyRCHtno7_FLYvIF2s-I136po2JNpPjdhSowCtweaxaeJTzf0fUltVQ5EQ8nhzYCBggVcVbAkausce_0Syn0haoGBEvKltgK7CsR-m7A" width="540">
+
+<p>* Above map is just given for reference only and actual transit time may vary
+
+<p>Each order of DRO Bundle will incur $49.99 flat rate shipping. Sorry, we cannot combined multiple orders in single shipping charge. E.g. if you order 3 DRO Bundles then you will need to pay shipping charge for every single DRO Bundle, the total shipping charges will be $49.99 times 3.
+
+<p>We ship to only to Contiguous U.S. states.  Order placed with shipping address other than in contiguous states will be cancelled.
+
+<p>We also don’t ship to P.O. boxes, order will be cancelled if the shipping address is P.O. Box
+
+<p>We will collect your payment (through PayPal) when your order is shipped and not while your order is in process. Order will shipped the same day if order is placed before 12 pm PST on that day. All orders placed after 12 pm will be shipped the next business day.
+
+<p>Shipping charges include the shipping insurance, if you find the items included in the box are damaged due to shipping then please send us the pictures of it within 24 hours of receiving delivery so that we can take the insurance claim with the FedEx. Any damage claim reported after 24 hrs after the receiving the delivery will be declined.');
 ?>
