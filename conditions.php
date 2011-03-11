86523d5c315f628aee5bb712074feaaef6cfcc13
dro <dro@dro-VirtualBox.(none)>
2011-03-10 19:37:20 -0800

    added returns and conditions of use

--- conditions.php
+++ conditions.php
@@ -13,6 +13,10 @@
   require('includes/application_top.php');
 
   require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_CONDITIONS);
+  require(DIR_WS_LANGUAGES . $language . '/' .FILENAME_WARRANTY);
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_RETURNS);
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_SHIPPING);
+
 
   $breadcrumb->add(NAVBAR_TITLE, tep_href_link(FILENAME_CONDITIONS));
 
@@ -24,8 +28,16 @@
 <div class="contentContainer">
   <div class="contentText">
     <?php echo TEXT_INFORMATION; ?>
+    <hr style="width: 960px; height: 5px; color: rgb(9, 12, 9); border-style: solid; margin-left: 0px; margin-right: auto;" />
+    <h1> Warranty </h1>
+    <?php echo TEXT_WARRANTY_INFORMATION; ?>
+    <hr style="width: 960px; height: 5px; color: rgb(9, 12, 9); border-style: solid; margin-left: 0px; margin-right: auto;" />
+    <h1> Returns </h1>
+    <?php echo TEXT_RETURNS_INFORMATION; ?>
+    <hr style="width: 960px; height: 5px; color: rgb(9, 12, 9); border-style: solid; margin-left: 0px; margin-right: auto;" />
+    <h1> Shipping </h1>
+    <?php echo TEXT_SHIPPING_INFORMATION; ?>
   </div>
-
   <div class="buttonSet">
     <span class="buttonAction"><?php echo tep_draw_button(IMAGE_BUTTON_CONTINUE, 'triangle-1-e', tep_href_link(FILENAME_DEFAULT)); ?></span>
   </div>
