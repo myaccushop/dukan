8012126301d6a8354e7b9269aa74cc7582f01ddd
Mahesh Asolkar <mahesh@mahesha.com>
2011-03-13 11:16:52 -0700

    Added 'features' box

---
+++ includes/languages/english/modules/boxes/bm_features.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_BOXES_FEATURES_TITLE', 'Features');
+  define('MODULE_BOXES_FEATURES_DESCRIPTION', 'Show product features');
+  define('MODULE_BOXES_FEATURES_BOX_TITLE', 'Features');
+?>
--- includes/modules/boxes/bm_specials.php
+++ includes/modules/boxes/bm_specials.php
@@ -33,12 +33,12 @@
     function execute() {
       global $HTTP_GET_VARS, $languages_id, $currencies, $oscTemplate;
 
-      $num_display = (MAX_RANDOM_SELECT_SPECIALS > 4) ? 4 : MAX_RANDOM_SELECT_SPECIALS;
+      $num_display = (MAX_RANDOM_SELECT_SPECIALS > 2) ? 2 : MAX_RANDOM_SELECT_SPECIALS;
 
       if (!isset($HTTP_GET_VARS['products_id'])) {
         $specials_products_query = tep_db_query ("select p.products_id, pd.products_name, p.products_price, p.products_tax_class_id, p.products_image, s.specials_new_products_price from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_SPECIALS . " s where p.products_status = '1' and p.products_id = s.products_id and pd.products_id = s.products_id and pd.language_id = '" . (int)$languages_id . "' and s.status = '1' order by s.specials_date_added desc limit " . $num_display);
         // if ($random_product = tep_random_select("select p.products_id, pd.products_name, p.products_price, p.products_tax_class_id, p.products_image, s.specials_new_products_price from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_SPECIALS . " s where p.products_status = '1' and p.products_id = s.products_id and pd.products_id = s.products_id and pd.language_id = '" . (int)$languages_id . "' and s.status = '1' order by s.specials_date_added desc limit " . MAX_RANDOM_SELECT_SPECIALS)) {
-        $data = '<div class="grid_24 bottom_box box_with_image alpha omega">' .
+        $data = '<div class="grid_12 bottom_box box_with_image alpha">' .
                 '  <div class="bottom_box_title">' . MODULE_BOXES_SPECIALS_BOX_TITLE . '</div>';
         $count = 1;
         while ($specials_product = tep_db_fetch_array($specials_products_query)) {
