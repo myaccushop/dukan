d04216335787b908e7e0193090cd25f27188d1c2
dro <dro@dro-VirtualBox.(none)>
2011-03-17 19:04:49 -0700

    added few faqs

--- includes/languages/english/faq.php
+++ includes/languages/english/faq.php
@@ -14,13 +14,30 @@ define('NAVBAR_TITLE', 'FAQ');
 define('HEADING_TITLE', 'Frequently Asked Questions');
 
 define('TEXT_FAQ_INFORMATION', '
-<p><a name="faq_1"></a>
-<b>Q:</b> Are we ready yet?<br>
-<b>A:</b> No. But we soon will be.
+<strong><br />
+  Why buy from AccuDRO Systems ?</strong><br /><strong>  <br />
+</strong>    3 Reasons<br />
+     + Our DROs are best in business with advance features like, 
+magnetic scale, download co-ordinates using RS232 connector and remote 
+control<br />
+     + When you buy a product from us,  you make us your partner in your venture, we will not let you down.<br />
+     + Our vendor is in business for last 10 years and their DROs are been used world wide by MNCs. <br />
+     + We back our products with 2 year warranty, all repair/replacement is handle in USA<br /><br />
+<strong></strong> <strong>How do we contact you ?</strong><br /><strong><br />
+ </strong> By email support@accudro.com or call 555.555.5555<br />
+<br /><strong>Are you USA base ?</strong><br /><strong>  
+  <br />
+  </strong>Yes, we are proud to be USA based company.<br />
+<span style="font-weight: bold;"><br />
+</span><strong>What Payments Do You Accept?
+	</strong>
+<p>  AccuDRO accepts all major credit cards through PayPal, as well as PayPal payments (PayPal is optional).</p>
+<br />
 
-<p><a name="faq_2"></a>
-<b>Q:</b> Are we ready yet?<br>
-<b>A:</b> No. But we soon will be.
+  <br />
+<br />
+<br />
+
 ');
 
 ?>
