7684eccaf6840a46493ce681f2b9ec1f8f723ffe
dro <dro@dro-VirtualBox.(none)>
2012-02-08 21:01:07 -0800

    Manual added

--- includes/languages/english/kb_manuals.php
+++ includes/languages/english/kb_manuals.php
@@ -13,5 +13,6 @@
 define('NAVBAR_TITLE', 'Manuals');
 define('HEADING_TITLE', 'Manuals');
 
-define('TEXT_INFORMATION', 'Put your KB Manuals here.');
+define('TEXT_INFORMATION', ' <br/> Lathe and Mill Kit DRO manual :&nbsp;&nbsp; <a href="manuals/PEARL_V3.pdf">   IAP3X-Pearl v3</a><sub>pdf <sub/>');
+
 ?>
