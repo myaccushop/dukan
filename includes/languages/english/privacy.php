1517fa38292bbff61a78aae9d7ac7d3c60bccbdd
dro <dro@dro-VirtualBox.(none)>
2012-02-11 18:22:35 -0800

    added privacy policy

--- includes/languages/english/privacy.php
+++ includes/languages/english/privacy.php
@@ -10,8 +10,82 @@
   Released under the GNU General Public License
 */
 
-define('NAVBAR_TITLE', 'Privacy Notice');
-define('HEADING_TITLE', 'Privacy Notice');
+define('NAVBAR_TITLE', 'Privacy Policy');
+define('HEADING_TITLE', 'Privacy Policy');
 
-define('TEXT_INFORMATION', 'Our privacy policy is simple - we will <strong>never ever </strong> share any of your information with any one.');
+define('TEXT_INFORMATION', '
+ <br /><strong>What information do we collect?</strong> <br />
+We collect information from you when you place an order, subscribe to our newsletter, respond to a survey or fill out a form.  <br />
+When ordering or registering on our site, as appropriate, you may be asked to enter your: name, e-mail address, mailing address or phone number.<br /><br />
+
+<strong>What do we use your information for?</strong> <br />Any of the information we collect from you may be used in one of the following ways: 
+  <ul>
+    <li>To personalize your experience (your information helps us to better respond to your individual needs)</li>
+    <li>To improve our website (we continually strive to improve our 
+website offerings based on the information and feedback we receive from 
+you)</li>
+    <li>To improve customer service (your information helps us to more 
+effectively respond to your customer service requests and support needs)</li>
+    <li>To process transactions</li>
+    <ul>
+      <li>Your information, whether public or private, will not be sold, exchanged, 
+transferred, or given to any other company for any reason whatsoever, 
+without your consent, other than for the express purpose of delivering 
+the purchased product or service requested.</li>
+    </ul>
+    <li>To send periodic emails</li>
+    <ul>
+      <li>The
+ email address you provide for order processing, may be used to send you
+ information and updates pertaining to your order, in addition to 
+receiving occasional company news, updates, related product or service 
+information, etc.</li>
+    </ul>
+  </ul>
+
+<br /><strong>How do we protect your information?</strong> <br />
+We implement a variety of security measures to maintain the safety of your personal information when you place an order: <br/> 
+<ul>
+    <li>We offer the use of a secure server. </li>
+    <li>All supplied information is transmitted via Secure Socket Layer (SSL) technology. </li>
+    <li>All our payment is handle by Paypal, so after a transaction, your private information (credit cards, social security 
+numbers, financials, etc.) will not be stored on our servers.</li>
+ </ul>
+
+
+<br /><strong>Do we use cookies?</strong> <br />
+Yes (Cookies are small files that a site or its service provider transfers 
+to your computers hard drive through your Web browser (if you allow) 
+that enables the sites or service providers systems to recognize your 
+browser and capture and remember certain information.<br /> <br />
+We use cookies to help us remember and process the items in your shopping cart,
+ understand and save your preferences for future visits, keep track of 
+advertisements and compile aggregate data about site traffic and site 
+interaction so that we can offer better site experiences and tools in 
+the future. We may contract with third-party service providers to assist
+ us in better understanding our site visitors. These service providers 
+are not permitted to use the information collected on our behalf except 
+to help us conduct and improve our business.<br /><br />
+If you prefer, you can choose to have your computer warn you each time a cookie is being 
+sent, or you can choose to turn off all cookies via your browser 
+settings. Like most websites, if you turn your cookies off, some of our 
+services may not function properly. However, you can still place orders 
+over the telephone or by contacting customer service.<br /><br /><strong>Do we disclose any information to outside parties?</strong> <br /><br />We
+ do not sell, trade, or otherwise transfer to outside parties your 
+personally identifiable information. This does not include trusted third
+ parties who assist us in operating our website, conducting our 
+business, or servicing you, so long as those parties agree to keep this 
+information confidential. We may also release your information when we 
+believe release is appropriate to comply with the law, enforce our site 
+policies, or protect ours or others rights, property, or safety. 
+However, non-personally identifiable visitor information may be provided
+ to other parties for marketing, advertising, or other uses.<br /><br /><strong>Online Privacy Policy Only</strong> <br /><br />This online privacy policy applies only to information collected through our website and not to information collected offline.<br />
+<br /><strong>Terms and Conditions</strong> <br /><br />Please
+ also visit our Terms and Conditions section establishing the use, 
+disclaimers, and limitations of liability governing the use of our 
+website at <a href="conditions.php">www.accudro.com</a><br /><br /><strong>Your Consent</strong> <br /><br />By using our site, you consent to our <a target="_blank" href="https://www.accudro.com/privacy.php" style="text-decoration: none; color: rgb(60, 60, 60);">privacy policy</a>.<br />
+<br /><strong>Changes to our Privacy Policy</strong> <br /><br />If
+ we decide to change our privacy policy, we will post those changes on 
+this page, and/or update the Privacy Policy modification date below. <br /><br />This policy was last modified on 2/11/2012<br /><br /><span></span><span></span><span></span></div>
+');
 ?>
