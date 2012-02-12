293957b628e109d6d22b31fb17434e7f89caac98
dro <dro@dro-VirtualBox.(none)>
2012-02-11 18:43:26 -0800

    updated T&C

--- includes/languages/english/conditions.php
+++ includes/languages/english/conditions.php
@@ -16,6 +16,9 @@ define('HEADING_TITLE', 'Conditions of Use');
 define('TEXT_INFORMATION', '
 <br />
 <h1><strong>Terms and Conditions</strong></h1>
+
+
+
 By accessing our website, you, the user, agree to the terms and 
 conditions we set out here. These terms and conditions may change at our
  discretion any time, and without notice. All orders and purchases made 
@@ -32,7 +35,19 @@ cancel orders at our discretion.<br />
 We will use commercially reasonable efforts to deliver items as quickly 
 as possible, and within any time indicated. However, we are not 
 responsible for delivery delays beyond our control. We reserve the right
- not to ship to certain addresses.<br />
+ not to ship to certain addresses. This
+ Agreement contains the complete terms and conditions that apply to you 
+in joining, buying, bidding, selling and all other activities you will 
+make in our website.  By using or shopping from this Web site, you agree
+ to be bound by its terms of use and shall comply thereof.  This 
+Agreement describes and encompasses the entire agreement between us and 
+you, and supersedes all prior or contemporaneous agreements, 
+representations, warranties and understandings with respect to the Site,
+ the content and computer programs provided by or through the Site, and 
+the subject matter of this Agreement. Amendments to this agreement can 
+be made and effected by us from time to time without specific notice to 
+you end. Agreement posted on the Site reflects the latest agreement and 
+you should carefully review the same before you use our site.<br />
 <br />
 <h1><strong>Our Liability</strong></h1>
 This website should be accessed and used at your own risk. Though 
@@ -67,6 +82,30 @@ AccuDRO Systems accepts the following forms of payment for online purchases:
 The transaction will be charged to your Paypal account only after we have 
 verified address, received payment authorization and confirmed stock availability, and your order is shipped. Your Paypal account will not be changed at the time you submit your order.<br /><br />
 
+
+<h1><strong>Editing, Deleting and Modification</strong></h1>
+We may edit, delete or modify any of the terms and conditions contained in
+ this Agreement, at any time and in our sole discretion, by posting a 
+notice or a new agreement on our site. YOUR CONTINUED PARTICIPATION IN 
+OUR PROGRAM, VISIT AND SHOPPING IN OUR SITE FOLLOWING OUR POSTING OF A 
+CHANGE NOTICE OR NEW AGREEMENT ON OUR SITE WILL CONSTITUTE BINDING 
+ACCEPTANCE OF THE CHANGE. <br /><br />
+
+
+<h1><strong>Fraud</strong></h1>
+FRAUDULENT ACTIVITIES are highly monitored in our site and if fraud is detected 
+AccuDRO Systems shall resort al remedies available to us, and you shall be 
+responsible for all costs and legal fees arising from these fraudulent 
+activities.<br /><br />
+
+
+<h1><strong>NON-WAIVER</strong></h1>
+Failure of the AccuDRO Systems to insist upon strict performance of any of the terms, conditions
+ and covenants hereof shall not be deemed a relinquishment or waiver of any rights or remedy that 
+the we may have, nor shall it be construed as a waiver of any subsequent breach of the terms, 
+conditions or covenants hereof, which terms, conditions and covenants shall continue to be in 
+full force and effect. No waiver by either party of any breach of any provision hereof shall be deemed a waiver of 
+any subsequent or prior breach of the same or any other provision.<br /><br />
 
 ');
 ?>
