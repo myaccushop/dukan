0db4edb7a34fabd76b329f082229159ecbcf587e
dro <dro@dro-VirtualBox.(none)>
2012-02-15 18:54:49 -0800

    added acrobate download info

--- includes/languages/english/kb_manuals.php
+++ includes/languages/english/kb_manuals.php
@@ -14,12 +14,26 @@ define('NAVBAR_TITLE', 'Manuals');
 define('HEADING_TITLE', 'Manuals');
 
 define('TEXT_INFORMATION', ' 
+<br/>
 <table> 
    <tr>  
      <td width="300">Lathe and Mill Kit DRO manual :</td>   
      <td width="500"><a style="color: #CC0000" href="manuals/PEARL_V3.pdf"><strong>   IAP3X-Pearl v3</strong> </a> <sub>(pdf) <sub/>  </td> 
   </tr>
-</table> ');
+</table> 
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<table border=0 cellpadding=0 cellspacing=0 width="50%">
+  <td><font size="1" > <strong>PDF Information</strong><br>Some of the links on this page are to PDF documents. To open PDF files you will need Adobe Acrobat Reader installed on your computer, it is available for free from <a href="http://www.adobe.com/products/acrobat/readstep2.html">Adobe.com.</a></font></td>
+  <td align="center"><a href="https://www.adobe.com/products/acrobat/readstep2.html" target="_blank" ><img src="images/getacrobat.gif" alt="Download Adobe Acrobat PDF Reader" width="88" height="31" vspace="5" hspace="5" border=0></td>
+</table>
+');
 
 ?>
 
