ee073b77a3eda74a9f5fdcbb2c308618fc9e517b
dro <dro@home-dro-VirtualBox.(none)>
2012-04-22 15:59:27 -0700

    added kb faq

--- includes/languages/english/kb_faq.php
+++ includes/languages/english/kb_faq.php
@@ -23,5 +23,39 @@ define('TEXT_INFORMATION', '
       the workpiece or tool has traveled..... .for more click <a href="http://blog.accudro.com/why-use-digital-readout-on-lathe-and-mill-mac" style="color:#cc0000" ><strong>here</strong></a><br />
     </p>
 
+
+
+ <strong>How to calculate ROI on the DRO kit?</strong><strong> </strong></p>
+    <p>
+      <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
+    </p>
+    <p style="margin-left: 40px;">Productivity improvement gained using DRO is
+      typically 20% to 40%.<br />
+      DRO Cost Justification (a typical Example) <sub>(*Cost of DRO varies
+        based on size of machine, and model of DRO.)</sub><br />
+    </p>
+    <ul>
+      <li>Typical Usage<br />
+      </li>
+      <ul>
+        <li>Lathe or Mill used hours per week: <strong>20 </strong></li>
+        <li>Shop Rate/Hour: <strong>$50</strong></li>
+        <li>DRO Productivity Improvement: <strong>40%</strong> (typically it is
+          20% to 40%)</li>
+        <li>Average Cost of a <strong>DRO kit @AccuDRO : $800</strong></li>
+      </ul>
+    </ul>
+    <ul>
+      <li>Improvement gained with DRO:</li>
+      <ul>
+        <li>Hours saved = <strong>8 hrs/week</strong></li>
+        <li>DRO Productivity improvement = <strong>$400 per week.</strong></li>
+        <li>Return of Investment in <strong>2 weeks</strong>.</li>
+      </ul>
+    </ul>
+    <p style="margin-left: 35px;">To customize above calculations please click  <a href="' . tep_href_link(FILENAME_DRO_PRODUCTIVITY_CAL) . '" style="color:#cc0000" ><strong>here</strong></a>.
+      
+
+
 ');
 ?>
