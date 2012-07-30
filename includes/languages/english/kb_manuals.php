fdc74ad8df2ac1e8256460b8eaadcd043a7af6f3
dro <dro@dro-VirtualBox.(none)>
2012-07-30 14:54:10 -0700

    Added xtreme manual and added images to pdf links in manual section

--- includes/languages/english/kb_manuals.php
+++ includes/languages/english/kb_manuals.php
@@ -18,18 +18,30 @@ define('TEXT_INFORMATION', '
 <table border="0" cellpadding="20"> 
    <tr bgcolor="#c1ecfa" >  
      <td width="300" height="75" >Lathe and Mill Kit DRO manual :</td>   
-     <td width="300"><a style="color: #CC0000" href="manuals/PEARL_V3.pdf"><strong>   IAP3X-Pearl v3</strong> </a> <sub>(pdf) <sub/> </td> 
+     <td width="300"><a style="color: #CC0000" href="manuals/PEARL_V3.pdf"> <img width="67" height="94" title="Accudro Pearl V3 manual" alt="Accudro Pearl V3 manual" src="images/accudro_pearl_v3.png">  </a> <sub>(pdf) <sub/> </td> 
   </tr>
    <tr bgcolor="#d5ffff" >  
      <td width="300" height="75">Lathe Machine Read Head Installation Manual: <br/> 
  <sub>(Details about how to mount x-axis and y-axis read heads) <sub/></td>   
-     <td width="300"><a style="color: #CC0000" href="manuals/accudro_lathe_instructions.pdf"><strong>   Lathe-Installation-Instruction</strong> </a> <sub>(pdf) <sub/>  </td> 
+     <td width="300"><a style="color: #CC0000" href="manuals/accudro_lathe_instructions.pdf">  <img width="67" height="94" title="Accudro Lathe-Installation-Instruction" alt="Accudro Lathe-Installation-Instruction" src="images/accudro_lathe_instructions.png">   </a> <sub>(pdf) <sub/>  </td> 
   </tr>
    <tr  bgcolor="#c1ecfa">  
      <td width="300" height="75">Mill Machine Read Head Installation Manual: <br/> 
  <sub>(Details about how to mount x-axis, y-axis and z-axis  read heads) <sub/></td>   
-     <td width="300"><a style="color: #CC0000" href="manuals/accudro_mill_instructions.pdf"><strong>   Mill-Installation-Instruction</strong> </a> <sub>(pdf) <sub/>  </td> 
+     <td width="300"><a style="color: #CC0000" href="manuals/accudro_mill_instructions.pdf">        <img width="67" height="94" title="Accudro Mill-Installation-Instruction" alt="Accudro Mill-Installation-Instruction" src="images/accudro_mill_instructions.png">
+   </a> <sub>(pdf) <sub/>  </td> 
   </tr>
+   <tr bgcolor="#d5ffff" >  
+     <td width="300" height="75" >Xterme+ DRO manual :</td>   
+     <td width="300">
+        <a style="color: #CC0000" href="manuals/AccuDRO_xtreme+_dro_manual.pdf">
+        <img width="67" height="94" title="Accudro Xterme+ DRO manual" alt="Accudro Xterme+ DRO manual" src="images/Accudro_xtreme+_dro_manual.png">
+        </a> 
+        <sub>(pdf) <sub/> 
+      </td> 
+   </tr>
+
+
 
 </table> 
 <br/>
